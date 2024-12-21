88aa8dfda070874ce1962dfedb6ec4e1f7d35db4
GamingSzene <126719741+GamingSzene@users.noreply.github.com>
2024-12-21 23:17:17 +0100

    Initial commit: Add website files

---
+++ Hauptseite/script.js
@@ -0,0 +1,27 @@
+const themeButton = document.getElementById('theme-toggle');
+const body = document.body;
+let isDarkMode = true;
+
+// Theme Toggle
+themeButton.addEventListener('click', () => {
+    if (isDarkMode) {
+        body.style.backgroundColor = '#030303';
+        body.style.color = '#fff';
+        themeButton.textContent = '☀️';
+    } else {
+        body.style.backgroundColor = '#fff';
+        body.style.color = '#030303';
+        themeButton.textContent = '🌙';
+    }
+    isDarkMode = !isDarkMode;
+});
+
+// Mausrad Scroll für horizontales Bewegen
+const container = document.querySelector('.container');
+window.addEventListener('wheel', (e) => {
+    if (e.deltaY !== 0) {
+        container.scrollLeft += e.deltaY; // Scrollt horizontal statt vertikal
+        e.preventDefault();
+    }
+})
+
