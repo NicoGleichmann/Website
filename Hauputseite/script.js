07b136d51444239a1926395e9bb7fc8528c031d9
Nico Gleichmann <nicogleichmann39@gmail.com>
2025-01-04 22:04:02 +0100

    Website stand 22:00

--- Hauputseite/script.js
+++ Hauputseite/script.js
@@ -18,6 +18,10 @@ themeButton.addEventListener('click', () => {
 
 // Mausrad Scroll für horizontales Bewegen
 const container = document.querySelector('.container');
+let isDragging = false; //für ziehen
+let startX; //für ziehen
+let scrollLeft; //für ziehen
+
 window.addEventListener('wheel', (e) => {
     if (e.deltaY !== 0) {
         container.scrollLeft += e.deltaY; // Scrollt horizontal statt vertikal
@@ -26,7 +30,47 @@ window.addEventListener('wheel', (e) => {
 })
 
 
-// alert("Nutze das Mausrad zum scrollen!");
+container.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    container.classList.add("active");
+    startX = e.pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+  });
+
+  container.addEventListener("mouseleave", () => {
+    isDragging = false;
+    container.classList.remove("active");
+  });
+
+  container.addEventListener("mouseup", () => {
+    isDragging = false;
+    scrollContainer.classList.remove("active");
+  });
+
+  container.addEventListener("mousemove", (e) => {
+    if (!isDragging) return;
+    e.preventDefault();
+    const x = e.pageX - container.offsetLeft;
+    const walk = (x - startX) * 2; // Adjust scroll sensitivity
+    container.scrollLeft = scrollLeft - walk;
+  });
+
+
+// Alert
+//alert("Nutze das Mausrad zum scrollen des Bildschirms!");
+
+// Links für Container
+const emailbox = document.getElementById("email_box");
+const instabox = document.getElementById("insta");
+
+emailbox.addEventListener("click", () => {
+    window.location.href = "https://mail.google.com/mail/u/0/?pli=1#inbox";
+});
+
+instabox.addEventListener("click", () => {
+    window.location.href = "https://www.instagram.com/nico.gleichmann/";
+});
+
 
 
 
